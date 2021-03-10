8b187029404bd973ef4812476808226b0cf3a5d1
AKN-YASUDA <hbbpf766@yahoo.co.jp>
2021-03-10 17:44:26 +0900

    STEP1 手順６

---
+++ spec/factories/form_object.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :form_object do
+    user_id { 2 }
+    item_id { 2 }
+    postcode { '123-4567' }
+    prefecture_id { 2 }
+    town { '東村山市' }
+    house_number { '1-1' }
+    building_name { "マンション東京" }
+    phone { '09012345678' }
+  end
+end
+
