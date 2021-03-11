cd3fe40cfca6368428eebbbc018af3d0675c9ac6
AKN-YASUDA <hbbpf766@yahoo.co.jp>
2021-03-12 08:43:16 +0900

    住所・item情報登録

--- app/controllers/purchasings_controller.rb
+++ app/controllers/purchasings_controller.rb
@@ -1,5 +1,7 @@
 class PurchasingsController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :contributor_confirmation, only: [:index, :create]
 
   def index
     @purchasing = FormObject.new
@@ -8,8 +10,9 @@ class PurchasingsController < ApplicationController
   def create
     @purchasing = FormObject.new(purchasing_params)
     if @purchasing.valid?
+      pay_item
       @purchasing.save
-      redirect_to item_purchasings_path
+      redirect_to root_path
     else
       render :index
     end
@@ -21,7 +24,20 @@ class PurchasingsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
- def purchasing_params
-  params.require(:form_object).permit(:item_id, :user_id, :postcode, :prefecture_id, :town, :house_number, :building_name, :phone)
- end
+  def purchasing_params
+    params.require(:form_object).permit(:item_id, :user_id, :postcode, :prefecture_id, :town, :house_number, :building_name, :phone).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: purchasing_params[:token],    
+      currency: 'jpy' 
+    )
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
 end
--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -1,6 +1,6 @@
 class FormObject
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :town, :house_number, :building_name, :phone
+  attr_accessor :token, :user_id, :item_id, :postcode, :prefecture_id, :town, :house_number, :building_name, :phone
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -10,10 +10,12 @@ class FormObject
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :user_id
     validates :item_id
+    validates :token
     end
 
   def save
-    Address.create(postcode: postcode, prefecture_id: prefecture_id, town: town, house_number: house_number, building_name: building_name, phone: phone)
-    Purchasing.create(item_id: item_id, user_id: user_id)
+    @purchasing = Purchasing.create(item_id: item_id, user_id: user_id)
+    Address.create(purchasing_id: @purchasing.id, postcode: postcode, prefecture_id: prefecture_id, town: town, house_number: house_number, building_name: building_name, phone: phone)
+   
   end
 end
