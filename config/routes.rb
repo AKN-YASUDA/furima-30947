0b469f3f7f0bf715562a1127c60e10bcc725b7a2
AKN-YASUDA <hbbpf766@yahoo.co.jp>
2021-04-04 23:30:00 +0900

    card_ルーティング、モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
   resources :items do
     resources :purchasings, only: [:index, :create]
   end
